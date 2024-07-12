f7db5f6b77a40802b761f2794da5ee5b879c4cd0
Gillian <Gillian@LTMACTECH017.local>
2024-07-12 14:01:28 +0300

    Adding the plant logic and webpack configurations

---
+++ src/plant.js
@@ -0,0 +1,39 @@
+// src/plant.js
+
+function createPlant(name) {
+    let state = {
+      name,
+      height: 0,
+      abilities: []
+    };
+  
+    return {
+      getState: () => ({ ...state }), // Return a copy of the state
+      grow: () => {
+        state.height += 1;
+      },
+      addAbility: (ability) => {
+        state.abilities.push(ability);
+      }
+    };
+  }
+  
+  const waterable = (plant) => {
+    const originalGrow = plant.grow;
+    plant.grow = () => {
+      originalGrow();
+      console.log(`${plant.getState().name} has been watered and grown!`);
+    };
+  };
+  
+  const fertilizerable = (plant) => {
+    const originalGrow = plant.grow;
+    plant.grow = () => {
+      originalGrow();
+      originalGrow();
+      console.log(`${plant.getState().name} has been fertilized and grown twice!`);
+    };
+  };
+  
+  module.exports = { createPlant, waterable, fertilizerable };
+  
